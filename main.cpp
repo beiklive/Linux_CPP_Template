3012af77ad0c2c786700ebce0b2eae009ebbe322
beiklive <1376896787@qq.com>
2022-05-23 23:03:13 +0800

    add nlohmann/json
    Signed-off-by: beiklive <1376896787@qq.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <hello.h>
 #include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
+using nlohmann::json;
 int main(){
     std::cout << "App start ~~" << std::endl;
     Hello();
     test();
     spdlog::info("spdlog init");
+    json j;
+    j["pi"] = 3.141;
+    j["happy"] = true;
+    j["name"] = "Niels";
+    j["nothing"] = nullptr;
+    j["answer"]["everything"] = 42;
+    j["list"] = { 1, 0, 2 };
+    j["object"] = { {"currency", "USD"}, {"value", 42.99} };
+    std::cout << j << '\n';
+    
+
     return 0;
 }
