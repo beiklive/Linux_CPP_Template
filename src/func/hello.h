b670801d81653df3efb0252eaec556c2ac02928a
beiklive <1376896787@qq.com>
2022-05-28 10:14:02 +0800

    增加httplib  dpool
    Signed-off-by: beiklive <1376896787@qq.com>

--- src/func/hello.h
+++ src/func/hello.h
@@ -1,7 +1,8 @@
 #ifndef __HELLO_H__
 #define __HELLO_H__
 #include <iostream>
-
+#include <nlohmann/json.hpp>
+using nlohmann::json;
 void Hello();
 void test();
 #endif
--- src/func/world.cpp
+++ src/func/world.cpp
@@ -1,5 +1,13 @@
 #include "hello.h"
 
 void test(){
-    std::cout << "this is test" << std::endl;
+    json j;
+    j["pi"] = 3.141;
+    j["happy"] = true;
+    j["name"] = "Niels";
+    j["nothing"] = nullptr;
+    j["answer"]["everything"] = 42;
+    j["list"] = { 1, 0, 2 };
+    j["object"] = { {"currency", "USD"}, {"value", 42.99} };
+    std::cout << j << '\n';
 }
