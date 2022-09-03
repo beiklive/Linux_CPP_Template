6c4e50f2b1e72d98c6aeccf45140ebde87e00146
beiklive <1376896787@qq.com>
2022-09-03 11:39:31 -0700

    修改示例
    Signed-off-by: beiklive <1376896787@qq.com>

--- include/hello.h
+++ include/hello.h
@@ -1,6 +1,7 @@
 #ifndef __HELLO_H__
 #define __HELLO_H__
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 void Hello();
 #endif
--- src/hello.cpp
+++ src/hello.cpp
@@ -1,5 +1,12 @@
 #include "hello.h"
 
 void Hello(){
+    nlohmann::json j;
+    j["pi"] = 3.141;
+    j["happy"] = true;
+    j["name"] = "Niels";
+    j["nothing"] = nullptr;
+    std::cout << j << std::endl;
     std::cout << "hello world" << std::endl;
+
 }
